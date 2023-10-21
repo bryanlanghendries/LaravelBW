e378b7c36ac7205c6d31c0e743d31ea0a4a089a0
Bryan Langhendries <bryan.langhendries@student.ehb.be>
2023-10-21 15:15:49 +0200

    Added models and controllers

--- yappin/database/migrations/2023_10_21_130437_create_f_a_q_items_table.php
+++ yappin/database/migrations/2023_10_21_130437_create_f_a_q_items_table.php
@@ -11,14 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('f_a_q_items', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->boolean('is_admin')->default(false);
-            $table->rememberToken();
+            $table->integer('category_id');
+            $table->text('question');
+            $table->text('answer')->nullable();
             $table->timestamps();
         });
     }
@@ -28,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('f_a_q_items');
     }
 };
