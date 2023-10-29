f28230d80f776ad1a1b2a4b25ae11e44f8cabba6
Bryan Langhendries <bryan.langhendries@student.ehb.be>
2023-10-29 15:51:37 +0100

    Update show.blade.php

--- yappin/resources/views/posts/show.blade.php
+++ yappin/resources/views/posts/show.blade.php
@@ -3,10 +3,11 @@
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
-            @if (session('status'))
-                <div class="alert alert-success" role="alert"> {{ session('status') }} </div>
-            @endif
+
             <div class="col-md-8">
+                @if (session('status'))
+                    <div class="alert alert-success" role="alert"> {{ session('status') }} </div>
+                @endif
 
                 <x-card :post="$post" />
 
