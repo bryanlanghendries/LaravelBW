fc8a4dcfad7a0b8b2d977dc394d22d35e4478b54
Bryan Langhendries <bryan.langhendries@student.ehb.be>
2023-12-30 14:34:34 +0100

    Update about.blade.php

--- yappin/resources/views/about/about.blade.php
+++ yappin/resources/views/about/about.blade.php
@@ -5,6 +5,9 @@
     <div class="row justify-content-center">
         <h1>READ ME</h1>
         <a href="https://github.com/bryanlanghendries/LaravelBW"><h5>GITHUB LINK</h5></a>
+        <a href="https://ehb.instructuremedia.com/embed/edeb8a9e-3e8f-4af3-85f9-8be5ae754e7d"><h5>CANVAS VIDEO LINK</h5></a>
+        <a href="https://laravel.com/docs/10.x/"><h5>LARAVEL DOCS LINK</h5></a>
+        <a href="https://getbootstrap.com/"><h5>BOOTSTRAP DOCS LINK</h5></a>
     </div>
 </div>
 
